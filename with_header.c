9929c62a4bf27b706e4316a3411362820e865e38
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-04-14 16:35:04 +0000

    random thing

---
+++ with_header.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+	int count;
+	int capacity;
+} Header;
+
+
+
+int main() {
+	Header header;
+	header.count = 0;
+	header.capacity = 0;
+
+	int *arrr = NULL;
+
+	return 0;
+}
