477dd5647e7be3bc4f4d50a64a3ac714fe3afaac
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-04-15 10:35:37 -0700

    Made the macros work

--- with_header.c
+++ with_header.c
@@ -6,61 +6,44 @@ typedef struct {
 	size_t capacity;
 } Header;
 
+#define ARR_INITIAL_CAPACITY 256
 #define arrSize(arr) (((Header*) (arr)) - 1)->count
 
-#define arrPush(arr, val) \
-	do { \
-		if(arr == NULL) {
-			size_t iC = 256;
-			Header* header = malloc(sizeof(Header) + iC * sizeof(*arr));
+#define arrPush(arr, val) do { \
+	if(arr == NULL) { \
+		Header* header = malloc(sizeof(Header) + ARR_INITIAL_CAPACITY * sizeof(*arr)); \
+		header->count = 0; \
+		header->capacity = ARR_INITIAL_CAPACITY; \
+		arr = (void*) (header + 1); \
+	} \
+	\
+	Header* header = (Header*) (arr) - 1; \
+\
+	if(header->count >= header->capacity) { \
+		header->capacity *= 2; \
+		header = realloc(header, sizeof(Header) + header->capacity * sizeof(*arr)); \
+		arr = (void*) (header + 1); \
+	} \
+\
+	(arr)[header->count++] = (val); \
+} while(0)
 
-			header->count = 0;
-			header->capacity = iC;
-
-			arr = (void*) (header + 1);
-		}
-
-		Header* header = ((Header*) (arr) - 1);
-
-
-	} while(0) \
-
-int* arrPush(int* arr, int val) {
-	if(arr == NULL) {
-		// Initialize
-		size_t initialCapacity = 256;
-
-		Header* head = malloc(sizeof(Header) + initialCapacity * sizeof(int));
-
-		head->count = 0;
-		head->capacity = initialCapacity;
-
-		arr = (int*) (head + 1);
-	}
-
-	Header* header = ((Header*) arr) - 1;
-
-	if(header->count == header->capacity) {
-		header->capacity *= 2;
-		header = realloc(header, sizeof(Header) + header->capacity * sizeof(int));
-	}
-
-	arr[header->count] = val;
-	header->count++;
-
-	return (int*) (header + 1);
-}
+#define arrFree(arr) free((Header*) (arr) - 1)
 
 int main() {
 	int *arr = NULL;
 
-	arr = arrPush(arr, 3);
-	arr = arrPush(arr, 5);
+	arrPush(arr, 3);
+	arrPush(arr, 5);
 	
+	printf("[");
 	for(size_t i=0; i < arrSize(arr); ++i) {
-		printf("%d, ", arr[i]);
+		printf("%d%s", arr[i], i == arrSize(arr) - 1 ? "" : ", ");
 	}
+	printf("]");
 	printf("\n");
+
+	arrFree(arr);
 
 	return 0;
 }
